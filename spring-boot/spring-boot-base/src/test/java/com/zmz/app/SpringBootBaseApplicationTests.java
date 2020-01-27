e94f3239b8884bf4580ea19397ae8d45f129fb3e
Frank <1126028327@qq.com>
2020-01-27 17:04:33 +0800

    spring-boot-base 完善测试部分

--- spring-boot/spring-boot-base/src/test/java/com/zmz/app/SpringBootBaseApplicationTests.java
+++ spring-boot/spring-boot-base/src/test/java/com/zmz/app/SpringBootBaseApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * @author ASNPHDG
+ * @create 2020-01-27 15:15
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootBaseApplicationTests {
