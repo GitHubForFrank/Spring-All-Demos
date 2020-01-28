cb5be5a2d0d416482b092646b7ab02aa4176face
Frank <1126028327@qq.com>
2020-01-28 16:52:18 +0800

    spring-boot-websocket 完善测试部分

--- spring-boot/spring-boot-websocket/src/test/java/com/zmz/app/SpringBootWebsocketApplicationTests.java
+++ spring-boot/spring-boot-websocket/src/test/java/com/zmz/app/SpringBootWebsocketApplicationTests.java
@@ -1,16 +1,18 @@
 package com.zmz.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author ASNPHDG
  * @create 2020-01-27 17:34
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT, classes={SpringBootWebsocketApplication.class})
 public class SpringBootWebsocketApplicationTests {
 
     @Test
